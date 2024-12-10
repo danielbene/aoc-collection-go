75da40a1aa42c57fc86ab0f93c02b4d22152bbe7
Dániel Bene <danielbene@protonmail.com>
2024-12-10 14:46:02 +0000

    update deepcopy

--- util/matrixutil/helpers.go
+++ util/matrixutil/helpers.go
@@ -4,7 +4,7 @@ func RemoveMatrixRow[T any](slice [][]T, idx int) [][]T {
 	return append(slice[:idx], slice[idx+1:]...)
 }
 
-func DeepCopy(src [][]string) [][]string {
+func DeepCopy[T ~[][]string](src T) T {
 	dst := make([][]string, len(src))
 	for idx := range src {
 		dst[idx] = make([]string, len(src[idx]))
