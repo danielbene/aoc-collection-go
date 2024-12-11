11fd9373bdbe9ae648c81e1d44ded1f469840e08
Dániel Bene <danielbene@protonmail.com>
2024-12-11 09:03:20 +0000

    add solution for 2024-11

--- solutions/2024/11/main.go
+++ solutions/2024/11/main.go
@@ -44,7 +44,7 @@ func Part2(puzzleInput string) (solution int) {
 	return blink_o_matic(75)
 }
 
-func blink_o_matic(count int) int {
+func blink_o_matic(count int) (solution int) {
 	var stones []uint64
 	for _, line := range input.Lines {
 		tmp := strings.Split(line, " ")
@@ -54,28 +54,61 @@ func blink_o_matic(count int) int {
 		}
 	}
 
-	for i := 0; i < count; i++ {
-		fmt.Printf("loop: %d\n", i)
-		blink(&stones)
+	// key: stone value, value: counter
+	// instead of keeping track of individual stones we manage a ledger with stone counters
+	ledger := make(map[uint64]int)
+	for _, stone := range stones {
+		ledger[stone]++
 	}
 
-	return len(stones)
+	// doin da blinkers
+	for i := 0; i < count; i++ {
+		blink(&ledger)
+	}
+
+	for _, v := range ledger {
+		solution += v
+	}
+
+	return solution
 }
 
-func blink(stones *[]uint64) {
-	for idx, stone := range *stones {
-		strStone := strconv.Itoa(int((*stones)[idx]))
+func blink(ledger *map[uint64]int) {
+	keys := getStones(ledger)         // getting stones with at least one counter
+	newStones := make(map[uint64]int) // tmp map for new stones to avoid inloop modification of the ledger
+
+	for _, stone := range keys {
+		strStone := strconv.Itoa(int(stone)) // probably not optimal but convenient
 		digitCnt := len(strStone)
+
+		cnt := (*ledger)[uint64(stone)]
+		(*ledger)[uint64(stone)] = 0
+
 		if stone == 0 {
-			(*stones)[idx] = 1
+			newStones[1] += cnt
 		} else if digitCnt%2 == 0 {
 			a, _ := strconv.Atoi(strStone[:digitCnt/2])
 			b, _ := strconv.Atoi(strStone[digitCnt/2:])
 
-			(*stones)[idx] = uint64(a)
-			*stones = append(*stones, uint64(b)) // putting in the back to avoid idx duckup
+			newStones[uint64(a)] += cnt
+			newStones[uint64(b)] += cnt
 		} else {
-			(*stones)[idx] = (*stones)[idx] * 2024
+			newStones[uint64(stone*2024)] += cnt
 		}
 	}
+
+	// update ledger with new stones
+	for k, v := range newStones {
+		(*ledger)[uint64(k)] += v
+	}
+}
+
+func getStones(m *map[uint64]int) (keys []uint64) {
+	for k, v := range *m {
+		if v > 0 {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
 }
